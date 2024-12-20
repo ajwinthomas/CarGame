47e98e2ff4dba30db19ce471674fa51153dc6c22
ajwinthomas <thomasajwin@gmail.com>
2024-12-20 14:33:02 +0530

    Steering movement implemented
    Steering implemented and given the center of mass also.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -21,15 +21,22 @@ public class CarController : MonoBehaviour
     public float maxAceleration = 30.0f;
     public float brakeAceleration = 50.0f;
 
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
     public List<Wheel> Wheels;
 
     float moveInput;
+    float steerInput;
 
     private Rigidbody carRb;
 
     void Start()
     {
         carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
     }
     void Update()
     {
@@ -38,11 +45,13 @@ public class CarController : MonoBehaviour
     void LateUpdate()
     {
         Move();
+        Steer();
     }
 
     void GetInputs()
     {
         moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
     }
 
     void Move()
@@ -50,6 +59,18 @@ public class CarController : MonoBehaviour
         foreach (var wheel in Wheels)
         {
             wheel.wheelCollider.motorTorque = moveInput * 600 * maxAceleration * Time.deltaTime;
+        }
+    }
+
+    void Steer()
+    {
+        foreach(var wheel in Wheels)
+        {
+            if(wheel.axel == Axel.Front)
+            {
+                var   _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
         }
     }
 }
